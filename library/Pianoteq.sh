8a6fff6380c9f17600f5c702db54e767c0415783
Tai An Su <a@tai.su>
2024-11-28 14:35:51 +0800

    Update to Pianoteq8 with some fixes

--- library/Pianoteq.sh
+++ library/Pianoteq.sh
@@ -4,7 +4,7 @@ sudo cpufreq-set -c 1 -g performance
 sudo cpufreq-set -c 2 -g performance
 sudo cpufreq-set -c 3 -g performance
 
-/home/pi/'Pianoteq 6 STAGE'/arm/'Pianoteq 6 STAGE' --multicore max
+/home/pi/'Pianoteq 8'/arm-64bit/'Pianoteq 8' --multicore max
 
 sudo cpufreq-set -c 0 -g ondemand
 sudo cpufreq-set -c 1 -g ondemand
